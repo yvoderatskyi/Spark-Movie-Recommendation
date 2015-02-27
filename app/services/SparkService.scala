4eeea1df45528bbd828d12652026742f8509cbf2
yvoderatskiy <spawn8995@gmail.com>
2015-02-27 15:18:12 +0200

    Spark Context (Singleton-like)

--- app/services/SparkService.scala
+++ app/services/SparkService.scala
@@ -1,8 +1,21 @@
 package services
 
-/**
- * Created by yuriy on 2/27/15.
- */
-object SparkService {
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 
+object SparkService {
+  private lazy val context = {
+    val conf = new SparkConf(false)
+      .setAppName(SparkConfig.appName)
+      .setMaster(SparkConfig.master)
+    
+      for((key,value) <- SparkConfig.configs) {
+        conf.set(key, value)
+      }
+    
+    new SparkContext(conf)    
+  }
+  
+  def version = context.version
 }
